ca8c0159341fdc3657947ed7ea7f6fc713867ce0
SimonXIX <simon.bowie@protonmail.com>
2021-09-28 12:56:47 +0000

    Added cron schedule to SSL renewal scripts

--- sandstorm_ssl_renew.sh
+++ sandstorm_ssl_renew.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# @cron: Set to run on root crontab at 1300 everyday
+# 00 13 * * * /home/simonxix/docker/sandstorm_ssl_renew.sh >> /var/log/cron.log 2>&1
 
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# @cron: Set to run on root crontab at 1200 everyday
+# 00 12 * * * /home/simonxix/docker/ssl_renew.sh >> /var/log/cron.log 2>&1
 
 COMPOSE="/usr/local/bin/docker-compose --no-ansi"
 DOCKER="/usr/bin/docker"
